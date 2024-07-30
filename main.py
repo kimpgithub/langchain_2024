5878a4928bae58aa526a8b214d8f5de7d93ce10e
kimpgithub <kimkimkim365@gmail.com>
2024-07-30 10:04:44 +0900

    Add main.py, requirements.txt

---
+++ main.py
@@ -0,0 +1,30 @@
+# from dotenv import load_dotenv
+# load_dotenv()
+# import streamlit as st
+# from langchain.chat_models import ChatOpenAI
+
+# chat_model = ChatOpenAI()
+
+# st.title('AI 작사가')
+
+# content = st.text_input('작사할 주제를 제시해주세요。')
+
+# if st.button('작사 요청하기'):
+#     result = chat_model.invoke(content + "에 대한 노래의 작사를 해 줘")
+#     st.write(result.content)
+
+# from dotenv import load_dotenv
+# load_dotenv()
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+
+chat_model = ChatOpenAI()
+
+st.title('AI 작사가')
+
+content = st.text_input('작사할 주제를 제시해주세요。')
+
+if st.button('작사 요청하기'):
+    with st.spinner('시를 작성 중입니다...'):
+        result = chat_model.invoke(content + "에 대한 노래의 작사를 해 줘")
+        st.write(result.content)
